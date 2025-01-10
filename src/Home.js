440ac29e79aa04d0ca279f68f131a41265bbb9b2
srivarshithagatta <srivarshithagatta@gmail.com>
2025-01-10 09:53:41 +0530

    experiment_16

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,25 @@
 import { useState,useEffect } from 'react'
 import BlogList from './BlogList';
 const Home = () => {
-    const [name,setName]=useState("varshi");
-    const [blogs,setBlogs]=useState([
-    {title:"My Blog1", body:"Body of Blog1", author:"Ramu",id:1},
-    {title:"My Blog2", body:"Body of Blog2", author:"Raju",id:2},
-    {title:"My Blog3", body:"Body of Blog3", author:"Sindhu",id:3},
-    ]);
+    const [name,setName]=useState("Sindhu");
+    const [blogs,setBlogs]=useState(null);
     const handleDelete = (author)=>{
         const newblogs = blogs.filter(blog=>blog.author!==author)
         setBlogs(newblogs)
     };
     useEffect(() => {
-        console.log("use effect ran");
-        console.log(blogs)
-        console.log(name)
-    });
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+        return res.json();
+        })
+        .then(data => {
+        setBlogs(data);
+        })
+        },[]);
+    
     return (
     <div className="Home">
-            <button onClick={()=>setName("Yashu")}>Click Me</button>
-
-        <BlogList blogs = {blogs} title = "Hello varshi" handleDelete={handleDelete}/>
-
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
     );
    }
